c63d03b0e7920e1509ac59b0cc953be62e633c25
badjelix <miguel.ptfg@hotmail.com>
2018-09-26 16:19:18 +0100

    so para depois fazer o teu pull da camara q ja se ve a mesa

--- js/projeto.js
+++ js/projeto.js
@@ -4,6 +4,45 @@ var camera, scene, renderer;
 
 var geometry, material, mesh;
 
+var table, chair, lamp;
+
+function addTableLeg(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.CylinderGeometry(2.5, 2.5, 25);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addTableTop(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(80, 4, 40);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function createTable(x, y, z) {
+    'use strict';
+
+    table = new THREE.Object3D();
+
+    material = new THREE.MeshBasicMaterial({ color: 0x7c5100, wireframe: true });
+
+    addTableTop(table, 0, 0, 0);
+    addTableLeg(table, 37.5, -14, 17.5);
+    addTableLeg(table, 37.5, -14, -17.5);
+    addTableLeg(table, -37.5, 14, 17.5);
+    addTableLeg(table, -37.5, 14, -17.5);
+
+    scene.add(table);
+
+    table.position.x = x;
+    table.position.y = y;
+    table.position.z = z;
+}
+
 function createScene() {
     'use strict';
 
@@ -14,7 +53,7 @@ function createScene() {
 
 function createCamera() {
     'use strict';
-    camera = new THREE.OrthographicCamera(100,100,100,100);
+    camera = new THREE.OrthographicCamera(200,200,200,200);
 }
 
 function render() {
